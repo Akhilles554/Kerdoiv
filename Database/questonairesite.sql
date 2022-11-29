7ca6ee1f7888c11ec9e340899617fc552ed13197
Akhilles554 <82020078+Akhilles554@users.noreply.github.com>
2022-11-29 20:07:39 +0100

    Update questonairesite.sql

--- Database/questonairesite.sql
+++ Database/questonairesite.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- GÈp: 127.0.0.1:3306
--- LÈtrehoz·s ideje: 2022. Nov 29. 19:06
--- Kiszolg·lÛ verziÛja: 5.7.36
--- PHP verziÛ: 7.4.26
+-- G√©p: 127.0.0.1:3306
+-- L√©trehoz√°s ideje: 2022. Nov 29. 19:06
+-- Kiszolg√°l√≥ verzi√≥ja: 5.7.36
+-- PHP verzi√≥: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES latin1 */;
 
 --
--- Adatb·zis: `questonairesite`
+-- Adatb√°zis: `questonairesite`
 --
 CREATE DATABASE IF NOT EXISTS `questonairesite` DEFAULT CHARACTER SET latin2 COLLATE latin2_hungarian_ci;
 USE `questonairesite`;
@@ -26,12 +26,12 @@ USE `questonairesite`;
 -- --------------------------------------------------------
 
 --
--- T·bla szerkezet ehhez a t·bl·hoz `person`
+-- T√°bla szerkezet ehhez a t√°bl√°hoz `person`
 --
 
 DROP TABLE IF EXISTS `person`;
 CREATE TABLE IF NOT EXISTS `person` (
-  `ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Email` varchar(255) COLLATE latin2_hungarian_ci NOT NULL,
   `CatName` varchar(100) COLLATE latin2_hungarian_ci DEFAULT NULL,
   `ZipCode` int(11) DEFAULT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `person` (
 -- --------------------------------------------------------
 
 --
--- T·bla szerkezet ehhez a t·bl·hoz `questionaire`
+-- T√°bla szerkezet ehhez a t√°bl√°hoz `questionaire`
 --
 
 DROP TABLE IF EXISTS `questionaire`;
@@ -73,22 +73,22 @@ CREATE TABLE IF NOT EXISTS `questionaire` (
 -- --------------------------------------------------------
 
 --
--- T·bla szerkezet ehhez a t·bl·hoz `questionairehead`
+-- T√°bla szerkezet ehhez a t√°bl√°hoz `questionairehead`
 --
 
 DROP TABLE IF EXISTS `questionairehead`;
 CREATE TABLE IF NOT EXISTS `questionairehead` (
-  `ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `DeadLine` datetime NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin2 COLLATE=latin2_hungarian_ci;
 
 --
--- MegkˆtÈsek a kiÌrt t·bl·khoz
+-- Megk√∂t√©sek a ki√≠rt t√°bl√°khoz
 --
 
 --
--- MegkˆtÈsek a t·bl·hoz `questionaire`
+-- Megk√∂t√©sek a t√°bl√°hoz `questionaire`
 --
 ALTER TABLE `questionaire`
   ADD CONSTRAINT `FK_Questionaire_QuestionaireHeadID_QuestionaireHead` FOREIGN KEY (`QuestionaireHeadID`) REFERENCES `questionairehead` (`ID`),
